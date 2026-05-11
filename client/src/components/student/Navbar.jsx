99333228e236746df8d830ba2c11736207df651f
Vincent <efeohise@gmail.com>
2026-05-11 08:13:15 +0200

    first push

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { assets } from "../../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { AppContext } from "../../context/AppContext";
 import { toast } from "react-toastify";
 import axios from "axios";
