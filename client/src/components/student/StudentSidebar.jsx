5b6052041caf96340a96efb73077ec3a14930c2d
Vincent <efeohise@gmail.com>
2026-06-18 23:06:20 +0200

    dasb to sidebar

--- client/src/components/student/StudentSidebar.jsx
+++ client/src/components/student/StudentSidebar.jsx
@@ -5,6 +5,15 @@ import axios from "axios";
 
 const learningMenuItems = [
   {
+    name: "Dashboard",
+    path: "/student",
+    icon: (
+      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+      </svg>
+    ),
+  },
+  {
     name: "My Enrollments",
     path: "/student/enrollments",
     icon: (
