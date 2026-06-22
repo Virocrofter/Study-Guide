846de7fd92ac6b7f823e2f6b7022ee3cad13d682
Vincent <efeohise@gmail.com>
2026-06-22 22:17:02 +0200

    one line fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./context/AppContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 // Auth.js (OAuth) version:
 // No ClerkProvider is used anywhere. AppContextProvider must NOT call any Clerk hooks.
